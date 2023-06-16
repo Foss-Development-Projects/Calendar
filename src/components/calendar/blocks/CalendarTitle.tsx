586094513753638c0a3de44e15e965e5ea65bba6
Foster <fosterunlimited0123@gmail.com>
2023-06-16 14:33:20 +0530

    Calendar Initial Commit 2

--- src/components/calendar/blocks/CalendarTitle.tsx
+++ src/components/calendar/blocks/CalendarTitle.tsx
@@ -1,6 +1,6 @@
 import { BsFillCalendarWeekFill } from 'react-icons/bs'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
-import { GrFormNextLink, GrFormPreviousLink } from 'react-icons/gr';
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 
 const CalendarTitle = (props: any) => {
 	const size: number = 18;
@@ -12,17 +12,17 @@ const CalendarTitle = (props: any) => {
 					<h1 className="calendar-title-text">January 31, 2023</h1>
 				</section>
 				<section className="calendar-title-button-section">
-					<button className="calendar-button">
-						<GrFormPreviousLink className="calendar-button-icon" size={size} title="Previous Year" />
+					<button className="calendar-button calendar-button-year">
+						<BiLeftArrowAlt className="calendar-button-icon" size={size} title="Previous Year" />
 					</button>
-					<button className="calendar-button">
+					<button className="calendar-button calendar-button-month">
 						<FaAngleLeft className="calendar-button-icon" size={size} title="Previous Month" />
 					</button>
-					<button className="calendar-button">
+					<button className="calendar-button calendar-button-month">
 						<FaAngleRight className="calendar-button-icon" size={size} title="Next Month" />
 					</button>
-					<button className="calendar-button">
-						<GrFormNextLink className="calendar-button-icon" size={size} title="Next Year" />
+					<button className="calendar-button calendar-button-year">
+						<BiRightArrowAlt className="calendar-button-icon" size={size} title="Next Year" />
 					</button>
 				</section>
 			</div>
